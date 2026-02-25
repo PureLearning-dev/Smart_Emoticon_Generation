3dd7ea9254c9ecf7ea1465c7d36065ace346b9bd
PureLearning-dev <liu.jie.coder@gmail.com>
2026-02-25 19:58:42 +0800

    feat: 开启SpringBoot入口程序配置

--- smart_meter/src/main/java/com/purelearning/smart_meter/SmartMeterApplication.java
+++ smart_meter/src/main/java/com/purelearning/smart_meter/SmartMeterApplication.java
@@ -1,9 +1,13 @@
 package com.purelearning.smart_meter;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.purelearning.smart_meter.mapper")
+@ConfigurationPropertiesScan("com.purelearning.smart_meter.config.props")
 public class SmartMeterApplication {
 
     public static void main(String[] args) {
